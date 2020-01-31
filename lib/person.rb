cb17fb7ad0fca118758d34b04be6431348acf404
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-31 00:14:28 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,9 +2,9 @@ class Person
   #your code here
   attr_accessor :avi_attributes, :spencer_attributes
 
-  def initialize(avi_attributes: , spencer_attributes:)
-    @avi_attributes = avi_attributes
-    @spencer_attributes = spencer_attributes
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send(("#{key}="), value)
   end
 
 end
