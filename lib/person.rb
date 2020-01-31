14d954c805bc7bdccc8489986d2799869569b723
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-31 00:19:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
   #your code here
-  attr_accessor :name, :hair_color, :birthday, :weight, :eye_color, :height, :handed, :complexion, :t_shirt_size
+  attr_accessor :name, :hair_color, :birthday, :weight, :eye_color, :height, :handed, :complexion, :t_shirt_size, :wrist_size
 
   def initialize(attributes)
     attributes.each do |key, value|
