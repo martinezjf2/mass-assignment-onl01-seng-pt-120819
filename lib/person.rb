d93ec4ea770d1b202c7232171d1556bfb3fa2adf
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-31 00:10:05 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,8 +2,8 @@ class Person
   #your code here
   attr_accessor :avi_attributes, :spencer_attributes
 
-  def initialize(avi_attributes: , spencer_attributes: )
-    
+  def initialize(avi_attributes: , spencer_attributes:)
+
   end
 
 end
